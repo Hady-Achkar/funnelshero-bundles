67239db9c26d6760b9dba0d1910ad639747a4cfa
Mhmdhammoud <dr.co0obra@hotmail.com>
2021-12-16 18:31:36 +0300

    Raw

--- src/routes/publicRouter.ts
+++ src/routes/publicRouter.ts
@@ -3,5 +3,4 @@ import {GetAllBundles, TestingTrialWillEnd} from '../controllers'
 
 const router = express.Router()
 router.route('/').get(GetAllBundles)
-router.route('/trial-end').post(express.raw({type: 'application/json'}),TestingTrialWillEnd)
 export default router
