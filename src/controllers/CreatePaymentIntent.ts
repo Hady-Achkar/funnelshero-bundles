e33b141fe34f5c069a32b3b1333034753f593e3e
hady-achkar <hady.achkar@hotmail.com>
2022-04-15 17:47:01 +0300

    fixed

--- src/controllers/CreatePaymentIntent.ts
+++ src/controllers/CreatePaymentIntent.ts
@@ -6,78 +6,72 @@ import {UserState} from '../types'
 export default async (req: Request, res: Response) => {
 	//@ts-ignore
 	const {stripeId, _id: UserId} = req.user
-	if (req.method === 'POST') {
-		try {
-			const {priceId, paymentMethodId} = req.body
+	const {priceId, paymentMethodId} = req.body
 
-			if (!priceId || priceId === '') {
-				return res.status(400).json({
-					status: 'Failure',
-					errors: [
-						{
-							name: 'missing productPriceId',
-							field: 'productPriceId',
-						},
-					],
-					requestTime: new Date().toISOString(),
-				})
-			}
-			const _verifyUser = await Users.findById(UserId)
-			if (!_verifyUser) {
-				return res.status(400).json({
-					status: 'Failure',
-					message: 'User was not found',
-					requestTime: new Date().toISOString(),
-				})
-			}
-
-			const paymentMethod = await Stripe.paymentMethods.attach(
-				paymentMethodId,
-				{customer: stripeId}
-			)
-
-			const _customer = await Stripe.customers.retrieve(stripeId)
-
-			console.log(_customer)
-
-			//@ts-ignore
-			if (_customer?.invoice_settings?.default_payment_method === null) {
-				await Stripe.customers?.update(stripeId, {
-					invoice_settings: {
-						default_payment_method: paymentMethod?.id,
+	try {
+		if (!priceId || priceId === '') {
+			return res.status(400).json({
+				status: 'Failure',
+				errors: [
+					{
+						name: 'missing priceId',
+						field: 'priceId',
 					},
-				})
-			}
-			const subscription = await Stripe.subscriptions.create({
-				customer: stripeId,
-				items: [{price: priceId}],
+				],
+				requestTime: new Date().toISOString(),
 			})
-
-			const updatedUser = await Users.findByIdAndUpdate(
-				UserId,
-				{
-					$set: {
-						activeSubscription: subscription.id,
-						activePrice: priceId as string,
-						status: UserState.SUB_ACTIVE,
-					},
-				},
-				{
-					new: true,
-				}
-			).select('-password')
-			res.status(200).json({
-				message: 'Subscription was created successfully',
-				subscription: subscription.id,
-				user: updatedUser,
-			})
-		} catch (err) {
-			if (err instanceof Error) {
-				res.status(500).json({statusCode: 500, message: err.message})
-			}
 		}
-	} else {
-		res.setHeader('Allow', 'POST')
-		res.status(405).end('Method Not Allowed')
+		const _verifyUser = await Users.findById(UserId)
+		if (!_verifyUser) {
+			return res.status(400).json({
+				status: 'Failure',
+				message: 'User was not found',
+				requestTime: new Date().toISOString(),
+			})
+		}
+
+		const paymentMethod = await Stripe.paymentMethods.attach(paymentMethodId, {
+			customer: stripeId,
+		})
+
+		const _customer = await Stripe.customers.retrieve(stripeId)
+
+		console.log(_customer)
+
+		//@ts-ignore
+		if (_customer?.invoice_settings?.default_payment_method === null) {
+			await Stripe.customers?.update(stripeId, {
+				invoice_settings: {
+					default_payment_method: paymentMethod?.id,
+				},
+			})
+		}
+		const subscription = await Stripe.subscriptions.create({
+			customer: stripeId,
+			items: [{price: priceId}],
+		})
+
+		const updatedUser = await Users.findByIdAndUpdate(
+			UserId,
+			{
+				$set: {
+					activeSubscription: subscription.id,
+					activePrice: priceId as string,
+					status: UserState.SUB_ACTIVE,
+				},
+			},
+			{
+				new: true,
+			}
+		).select('-password')
+		res.status(200).json({
+			message: 'Subscription was created successfully',
+			subscription: subscription.id,
+			user: updatedUser,
+		})
+	} catch (err) {
+		if (err instanceof Error) {
+			res.status(500).json({statusCode: 500, message: err.message})
+		}
 	}
 }
