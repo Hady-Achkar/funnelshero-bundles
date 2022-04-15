267455107d073dd14fedb2c40af31933cbb3fe74
hady-achkar <hady.achkar@hotmail.com>
2022-04-15 17:13:17 +0300

    implemented

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -1,12 +1,19 @@
 import express from 'express'
-import {AddNewPaymentMethod, BundleSub, CreateCustomer, CreateNewBundle, GetCheckOutDetails} from '../controllers'
+import {
+	AddNewPaymentMethod,
+	CreateCustomer,
+	CreateNewBundle,
+	CreatePaymentIntent,
+	GetCheckOutDetails,
+} from '../controllers'
 import {ValidateNewBundle} from '../middlewares'
 
 const router = express.Router()
 
 router.route('/').post(ValidateNewBundle, CreateNewBundle)
 router.route('/customer').post(CreateCustomer)
-router.route('/sub').post(BundleSub)
+router.route('/sub').post(CreatePaymentIntent)
 router.route('/payment').post(AddNewPaymentMethod)
 router.route('/checkout').get(GetCheckOutDetails)
+
 export default router
--- src/routes/publicRouter.ts
+++ src/routes/publicRouter.ts
@@ -1,9 +1,17 @@
 import express from 'express'
-import {ConfigFront, GetAllBundles, TestingTrialWillEnd} from '../controllers'
+import {
+	ConfigFront,
+	GetAllBundles,
+	TestingTrialWillEnd,
+	CreatePaymentIntent,
+} from '../controllers'
 
 const router = express.Router()
 router.route('/').get(GetAllBundles)
-router.route('/trial-end').post(express.raw({type: 'application/json'}), TestingTrialWillEnd)
+router
+	.route('/trial-end')
+	.post(express.raw({type: 'application/json'}), TestingTrialWillEnd)
 router.route('/config').get(ConfigFront)
+router.route('/client-secret').post(CreatePaymentIntent)
 
 export default router
